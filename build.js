a7690777f522b1fe83ba1816c8857b34a337c351
Benjamin G <randomblock1@pm.me>
2022-04-28 16:45:58 -0500

    v1.0.1

--- build.js
+++ build.js
@@ -1,12 +1,14 @@
-var nativefier = require("nativefier").default;
-var process = require("process");
+const nativefier = require("nativefier").default;
+const fs = require("fs");
+const process = require("process");
 
 inputPlatform = process.argv[2];
+packagejson = fs.readFileSync("package.json");
 
 var options = {
   name: "Google Messages", // will be inferred if not specified
   targetUrl: "https://messages.google.com/web", // required
-  version: Date.now(),
+  version: JSON.parse(packagejson)["version"],
   out: "dist",
   counter: true,
   bounce: true,
